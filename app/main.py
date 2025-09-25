c7b612850115f6bccc3c190c6cd8055496b0d631
safanailmy <ilmysafana@gmail.com>
2025-09-25 16:51:48 +0530

    modified main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import users, items, interactions, recommend
 from app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Hyper-Personalization API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(users.router)
 app.include_router(items.router)
